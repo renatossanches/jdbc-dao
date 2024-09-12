6bac806eba723eea281aecf58ab8968c22f405dc
Renato Sanches <renato.sanches2004@gmail.com>
2024-09-12 12:58:27 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/modal/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package modal.dao;
+
+import java.util.List;
+
+import modal.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
