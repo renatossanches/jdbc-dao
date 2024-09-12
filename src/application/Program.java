f368d4452e4ab54dd38a53fa801b94e223619330
Renato Sanches <renato.sanches2004@gmail.com>
2024-09-12 12:49:56 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+
 import modal.entities.Department;
+import modal.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
 
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", LocalDate.now(), 3000.0, obj);
+		
+		System.out.println(seller);
+
+		
+		
 	}
 
 }
--- src/modal/entities/Department.java
+++ src/modal/entities/Department.java
@@ -14,7 +14,6 @@ public class Department implements Serializable{
 	}
 
 	public Department(Integer id, String name) {
-		super();
 		this.id = id;
 		this.name = name;
 	}
